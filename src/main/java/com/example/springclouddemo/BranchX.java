5f53c474e24b3c33e3679f98a797cd6c713b16af
Charan <Charan@host.docker.internal>
2024-07-14 12:41:18 +0530

    changes committed from branch x

---
+++ src/main/java/com/example/springclouddemo/BranchX.java
@@ -0,0 +1,14 @@
+package com.example.springclouddemo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BranchX {
+	
+	
+	public void gett()
+	{
+		System.out.println("naidu");
+	}
+
+}
